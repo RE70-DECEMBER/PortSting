d50e3ae144387b64a77e6c4e5efe2728d9b2ae95
RE70-DECEMBER <158739296+RE70-DECEMBER@users.noreply.github.com>
2025-06-28 12:10:10 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os/user"
+	"strings"
+	"sync"
+	"time"
+)
+
+var (
+	ip            = flag.String("t", "127.0.0.1", "Target ip address to scan")
+	portCounter   = 0
+	bannerCounter = 0
+)
+
+func scanner(ip string, port int, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	target := fmt.Sprintf("%s:%d", ip, port)
+
+	//Attempt TCP connection
+	conn, err := net.DialTimeout("tcp", target, 2*time.Second)
+
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	//Send The HTTP Request
+	if port == 80 || port == 8080 {
+		conn.Write([]byte("GET / HTTP/1.0\r\n\r\n"))
+	}
+
+	//Grab Banner
+	conn.SetReadDeadline(time.Now().Add(2 * time.Second))
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+
+	banner := "\033[31m Unknown \033[0m"
+	if err == nil && n > 0 {
+		response := string(buf[:n])
+		if port == 80 || port == 8080 {
+			lines := strings.Split(response, "\r\n")
+			for _, line := range lines {
+				if strings.HasPrefix(strings.ToLower(line), "server:") {
+					banner = strings.TrimSpace(line[len("server:"):])
+					break
+				}
+			}
+		} else {
+			banner = strings.TrimSpace(response)
+		}
+		bannerCounter++
+	}
+
+	//Shows found ports and counts how many ports there are
+	fmt.Printf("Found Port --> %v \tService Info: %v \n", port, banner)
+	portCounter++
+}
+
+func main() {
+	flag.Parse()
+
+	var wg sync.WaitGroup
+
+	currentUser, err := user.Current() //detect username
+
+	username := currentUser.Username //set username
+
+	if err != nil { // if fails to get username
+
+		username = "!!!MEOW MEOW Could Not Find Your Username! MEOW MEOW!!!"
+
+	}
+
+	fmt.Print("\033[34m")
+
+	fmt.Print(`
+  _____           _    _____ _   _             
+ |  __ \         | |  / ____| | (_)            
+ | |__) |__  _ __| |_| (___ | |_ _ _ __   __ _ 
+ |  ___/ _ \| '__| __|\___ \| __| | '_ \ / _  |
+ | |  | (_) | |  | |_ ____) | |_| | | | | (_| |
+ |_|   \___/|_|   \__|_____/ \__|_|_| |_|\__, |
+                                          __/ |
+                                         |___/ 
+`)
+
+	fmt.Print("\033[0m")
+	fmt.Printf("\033[35m Author: RE70-DECEMBER\nSpecial Thanks To Yassinox\nWelcome %v Glad Your Here :)\033[0m\n\n", strings.Title(username))
+	fmt.Printf("\033[34mScanning Target: %s\tThrough TCP Ports 1/65535 \033[0m\n\n", *ip)
+
+	for currentPort := 1; currentPort <= 65535; currentPort++ {
+		wg.Add(1)
+		go scanner(*ip, currentPort, &wg)
+	}
+
+	wg.Wait()
+	fmt.Println()
+
+	if portCounter <= 0 {
+		fmt.Println("\033[32mScanning Done! \033[0m -->\t\033[31m!!!MEOW MEOW\033[0m \033[32m--> No Found Ports Sorry :( <-- \033[31mMEOW MEOW!!!\033[0m\n\033")
+	} else {
+		fmt.Printf("\033[34mScanning Done! \033[0m \tFound Ports --> %v \tFound Banners: %v\n", portCounter, bannerCounter)
+	}
+}
