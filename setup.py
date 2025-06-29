58eae7b17e1fca5683bbc16afc21cec51d8d54f3
RE70-DECEMBER <158739296+RE70-DECEMBER@users.noreply.github.com>
2025-06-29 11:34:36 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,42 @@
-import os
+#Setup script made by NoFace For RE70-DECEMBER PortSting port scanner script!
+#Thanks NoFace - RE70-DECEMBER :)
 
-print("Welcome To portsting Setup!\n\n1. Make Work Outside Of DIR\n2. Quit\n")
+from os import system, chdir, path, environ, pathsep
 
-iN = input("enter choice: ")
+chdir(path.dirname(path.abspath(__file__)))
+path_dirs = environ.get("PATH", "").split(pathsep)
 
-if iN == "1" or iN == "one" or iN == "One" or iN == "y":
-	print("\nCopying portsting to --> /usr/local/bin")
-	os.system("sudo cp portsting /usr/local/bin")
-	print("Script Should Work Now OutSide Of Script DIR!\nTry Typing portsting in home DIR")
+if "/usr/local/bin" in path_dirs:
+    install_dir = '/usr/local/bin'
+elif "/opt" in path_dirs:
+    install_dir = '/opt'
 else:
-	print("\nScript Exited!")
+    install_dir = None
+
+print(f"""
+Welcome To portsting Setup! - Setup Script Made By NoFace :0 - PortSting By RE70-DECMBER :)
+
+1. Add to Path using existing folder ({install_dir})
+2. Add to Path by creating a new folder?
+3. Quit
+""")
+
+user_input = input("enter choice: ")
+
+if user_input.lower() in ['1', 'one']:
+    print(f'\nCopying portsting to --> {install_dir}')
+elif user_input.lower() in ['2', 'two']:
+    install_dir = input('new install directory: ')
+    system(f'mkdir -p {install_dir}')
+else:
+    exit()
+
+system(f'sudo cp ./portsting {install_dir}')
+if user_input.lower in ['1', 'one']:
+    print(f'Script has been added to systems path at {install_dir}/portsting')
+else:
+    print(f"""
+    add the following code snippet to your shell rc file (bashrc, zshrc, etc)
+    
+    export PATH="{install_dir}:$PATH"
+    """)
